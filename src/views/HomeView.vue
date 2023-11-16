8a1d98ffcfdaceaa1d747b374127a71e37e153fa
BalloutZeinab <zeinab.ballout@student.htw-berlin.de>
2023-11-16 19:30:24 +0100

    Milestone 3

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Zeinabs Website!"/>
   </div>
 </template>
 
@@ -9,6 +9,7 @@
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
+// css ab hier
 export default {
   name: 'HomeView',
   components: {
